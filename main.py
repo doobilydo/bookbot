a44e2454ad3ff36d8168e1434177e9b9ca04c56b
Jon <jonmhenrich@gmail.com>
2025-01-10 13:04:31 -0500

    Number of occurences of each character.

--- main.py
+++ main.py
@@ -3,8 +3,14 @@
 def main():
     path_to_file = "books/frankenstein.txt"
     text = get_text(path_to_file)
-    #print(get_number_of_words(text))
-    print(num_of_words(text))
+    print(f"--- Begin report of {path_to_file} ---")
+    print(f"{get_number_of_words(text)} words found in the document")
+    
+    chars = num_of_characters(text)
+    for key in chars:
+        print(f"The '{key}' character was found {chars[key]}")
+
+    print("--- End report ---")
     
     
 # Return text from a file.
@@ -16,7 +22,7 @@ def get_text(path_to_file):
 # Return the number of words in the string.
 def get_number_of_words(contents):
     words = contents.split()
-    return (len(words))
+    return len(words)
 
 # Return the count per unique word.
 def num_of_words(contents):
@@ -31,6 +37,20 @@ def num_of_words(contents):
             num_words[w] += 1
     
     return num_words
+# Return the count per unique character.
+def num_of_characters(contents):
+    words = contents.split()
+    num_characters = {}
+
+    for word in words:
+        for letter in word:
+            l = letter.lower()
+            if l not in num_characters:
+                num_characters[l] = 1
+            else:
+                num_characters[l] += 1
+    
+    return num_characters
 
 
 
