ef7cd69a492b5247a674172ea5288b2aa77360d6
Jon <jonmhenrich@gmail.com>
2025-01-10 12:51:05 -0500

    Number of occurences per word.

--- main.py
+++ main.py
@@ -3,7 +3,8 @@
 def main():
     path_to_file = "books/frankenstein.txt"
     text = get_text(path_to_file)
-    print(get_number_of_words(text))
+    #print(get_number_of_words(text))
+    print(num_of_words(text))
     
     
 # Return text from a file.
@@ -16,6 +17,21 @@ def get_text(path_to_file):
 def get_number_of_words(contents):
     words = contents.split()
     return (len(words))
+
+# Return the count per unique word.
+def num_of_words(contents):
+    words = contents.split()
+    num_words = {}
+
+    for word in words:
+        w = word.lower()
+        if w not in num_words:
+            num_words[w] = 1
+        else:
+            num_words[w] += 1
+    
+    return num_words
+
 
 
 # Run.
