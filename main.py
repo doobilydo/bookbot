6dccdc68580a853f0a803a5f107affc8d7eb08b5
Jon <jonmhenrich@gmail.com>
2025-01-10 12:42:53 -0500

    Refactor

--- main.py
+++ main.py
@@ -2,16 +2,19 @@
 
 def main():
     path_to_file = "books/frankenstein.txt"
-    print(get_number_of_words(path_to_file))
+    text = get_text(path_to_file)
+    print(get_number_of_words(text))
     
     
-    
-# Return the number of words in the string.
-def get_number_of_words(path_to_file):
+# Return text from a file.
+def get_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
-    
-    words = file_contents.split()
+    return file_contents
+
+# Return the number of words in the string.
+def get_number_of_words(contents):
+    words = contents.split()
     return (len(words))
 
 
